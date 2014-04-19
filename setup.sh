6a284f28874ff887cf489762b9f7de6535c645cb
Cameron Bates <cameron.r.bates@gmail.com>
2014-04-18 21:17:11 -0700

    Add radsrc directory to path in setup Also fix script directory location search

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 # 
 #   exec ./setup
 #
-export RADSRC_HOME=$1
+export RADSRC_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export RADSRC_DATA=$RADSRC_HOME/data
+export PATH=$RADSRC_HOME:$PATH
 
 
 
